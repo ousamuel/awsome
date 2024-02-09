833fb422ec39f3427b790fd823f17cfa1a9c6a90
Samuel <samuelou510@gmail.com>
2024-02-09 08:47:46 -0500

    aw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   const [text, setText] = useState<string>("60px");
   const [yes, setYes] = useState<string>("60px");
   const [success, setSuccess] = useState<boolean>(false);
+  const [slide, setSlide] = useState<boolean>(false);
 
   let yesOptions: string[] = [
     "yes",
@@ -26,20 +27,52 @@ export default function Home() {
 
   const clickNo = () => {
     setCounter((prev) => prev + 1);
-    setText((prev) => `${parseInt(prev) / 1.8}px`);
-    setYes((prev) => `${parseInt(prev) * 1.2}px`);
+    setText((prev) => `${parseInt(prev) / 1.6}px`);
+    setYes((prev) => `${parseInt(prev) * 1.1}px`);
+  };
+
+  const slideDown = () => {
+    setSlide(true);
   };
 
   return (
     <div className="flex flex-col text-center items-center bg-[#fcfaf8] w-[100vw] h-[100vh]">
-      <h1 className="uppercase">Will you be my valentines</h1>
+      <img
+        id="slider"
+        className={slide ? "her slide-in" : ""}
+        src="her.png"
+        alt="logo"
+        width={220}
+      />
+      <img
+        id="slider"
+        className={slide ? "her2 slide-in2" : ""}
+        src="her2.png"
+        alt="logo"
+        width={220}
+      />
+      <h1 className="uppercase text-[40px] text-rose-500">
+        Angela Wu, Will you be my valentines?
+      </h1>
       <img className="w-[300px]" src="/gifs/cat.gif" />
-      <img className={success ? "w-[300px]" : "hidden"} src="/gifs/tmp.gif" />
+      <div className={success ? "flex" : "hidden"}>
+        <img className="w-[300px]" src="/gifs/tmp.gif" />
+        <img
+          className={
+            slide
+              ? "hidden"
+              : "w-[100px] h-[100px] rounded-full cursor-pointer hover:bg-red-400 "
+          }
+          onClick={() => setSlide((prev) => !prev)}
+          src="/svgs/arrow-down.svg"
+        />
+      </div>
+
       <div className="min-w-[400px] justify-evenly">
         {/* {counter} */}
 
         {success ? null : (
-          <div className="">
+          <div className="flex flex-col">
             <button
               className="mx-5 pulsating-text"
               onClick={() => setSuccess(true)}
